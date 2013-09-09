432c4bcfbbec5ddce07a63e3aa94a3bab7620e13
Stefano Torresi <webdev@stefanotorresi.it>
2013-09-09 17:32:16 +0200

    initial commit

---
+++ jquery.anchorscroll.js
@@ -0,0 +1,45 @@
+(function($){
+
+    $.fn.anchorscroll = function(options) {
+
+        var defaults = {
+            speed: 700,
+            fx: "swing"
+        };
+
+        var options = $.extend(defaults, options);
+
+        return this.each(function(){
+            $(this).click(function(e) {
+                var href = $(this).attr("href");
+
+                if (href.indexOf("#") == -1) {
+                    return true;
+                }
+
+                var target = href.substring(href.indexOf("#"));
+
+                if (!$(target).length) {
+                    return true;
+                }
+
+                var destination = $(target).offset().top;
+
+                if (navigator.userAgent.match(/OS 5(_\d)+ like Mac OS X/i)) {
+                    $("body").append('<div id="ios5fix" style="height: 200px"></div>');
+                }
+
+                $(window).data('anchorscrolling', true);
+
+                $("html, body").stop(true).animate({scrollTop: destination}, options.speed, options.fx, function(){
+                    $(window).data('anchorscrolling', false);
+                    $("#ios5fix").remove();
+                });
+
+                return false;
+            });
+        });
+
+    };
+
+})(jQuery);
