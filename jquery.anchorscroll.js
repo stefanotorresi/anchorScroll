768bfd29b542c7a43c80cbf85df0d43c1507367c
Stefano Torresi <webdev@stefanotorresi.it>
2013-09-13 13:13:53 +0200

    separate actual scroll animation from anchor event handler

--- jquery.anchorscroll.js
+++ jquery.anchorscroll.js
@@ -1,18 +1,57 @@
 (function($){
 
-    $.fn.anchorscroll = function(options) {
+    $.fn.anchorScroll = function(options) {
 
         var defaults = {
             speed: 700,
             fx: "swing",
-            offset: 0
+            offset: 0,
+            updateHref: true,
+            callback: function(){}
         };
 
-        var options = $.extend(defaults, options);
+        this.options = $.extend(defaults, options);
+        var anchorScroll = this;
+
+        this.scrollToTarget = function(target) {
+
+            if (!$(target).length) {
+                return true;
+            }
+            var destination = $(target).offset().top;
+
+            if (anchorScroll.options.offset) {
+                destination += parseInt(anchorScroll.options.offset);
+            }
+
+            if (navigator.userAgent.match(/OS 5(_\d)+ like Mac OS X/i)) {
+                $("body").append('<div id="ios5fix" style="height: 200px"></div>');
+            }
+
+            $(window).data('anchorscrolling', true);
+
+            $("html, body").stop(true).animate({scrollTop: destination}, anchorScroll.options.speed, anchorScroll.options.fx, function(){
+                $(window).data('anchorscrolling', false);
+                $("#ios5fix").remove();
+                if (anchorScroll.options.updateHref) {
+                    anchorScroll.updateLocationHref();
+                }
+                anchorScroll.options.callback(target, anchorScroll);
+            });
+
+            return false;
+        }
+
+        this.updateLocationHref = function() {
+            if (anchorScroll.href) {
+                location.href = anchorScroll.href;
+            }
+        }
 
         return this.each(function(){
-            $(this).click(function(e) {
+            $(this).click(function() {
                 var href = $(this).attr("href");
+                anchorScroll.href = href;
 
                 if (href.indexOf("#") == -1) {
                     return true;
@@ -20,29 +59,7 @@
 
                 var target = href.substring(href.indexOf("#"));
 
-                if (!$(target).length) {
-                    return true;
-                }
-
-                var destination = $(target).offset().top;
-
-                if (options.offset) {
-                    destination += parseInt(options.offset);
-                }
-
-                if (navigator.userAgent.match(/OS 5(_\d)+ like Mac OS X/i)) {
-                    $("body").append('<div id="ios5fix" style="height: 200px"></div>');
-                }
-
-                $(window).data('anchorscrolling', true);
-
-                $("html, body").stop(true).animate({scrollTop: destination}, options.speed, options.fx, function(){
-                    $(window).data('anchorscrolling', false);
-                    $("#ios5fix").remove();
-                    location.href = href;
-                });
-
-                return false;
+                return anchorScroll.scrollToTarget(target);
             });
         });
 
